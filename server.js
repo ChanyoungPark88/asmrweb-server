3df46943fcaed9f47932717a96b0025d8ee4ba3a
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2022-09-14 05:42:53 -0600

    클라이언트 서버 분리

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const colors = require('colors');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+dotenv.config();
+connectDB();
+const app = express();
+const PORT = process.env.PORT;
+const messages = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// app.use('/api/messages', messageRoutes);
+
+app.get('/api', (req, res) => {
+  res.send('API is Running');
+});
+
+app.get('/api/messages', (req, res) => {
+  res.json(messages);
+});
+
+app.post('/api/messages', (req, res) => {
+  const messageLog = req.body.messageLog;
+  messageLog.forEach((e) => messages.push(e));
+});
+
+// Form here Realtime Socket PART
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.NODE_ENV,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} is joined room: ${data}`);
+  });
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data);
+    // console.log(data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
+    socket.leave(socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is Running on Port ${PORT}`.yellow.bold);
+  console.log(process.env.NODE_ENV);
+});
+
+module.exports = server;
